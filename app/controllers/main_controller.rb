e4684976d843c30716bde2478ae15c8b4e4ad5a2
Patrick Moody <patrick.n.moody@gmail.com>
2013-07-25 21:52:05 -0500

    add user views

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,8 +4,6 @@ class MainController < ApplicationController
     if session[:access_token]
       @client = Instagram.client(:access_token => session[:access_token])
       @user = @client.user
-      p "============================"
-      p @client.user_recent_media[0].caption.created_time
       @client.user_recent_media.each do |photo|
         temp_photo = Photo.find_or_initialize_by_url(caption: photo.caption.text, date: photo.caption.created_time.to_i, url:photo.images.standard_resolution.url)
         temp_photo.update_attributes(lat: photo.location.latitude, long: photo.location.longitude) if photo.location
@@ -13,7 +11,7 @@ class MainController < ApplicationController
       end
 
       @photos = Photo.order("date ASC")
-
     end
+
   end
 end
