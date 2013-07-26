e02120211feedda64eadf38fa323b35ec7841dd8
patmood <patrick.n.moody@gmail.com>
2013-07-25 23:19:02 -0500

    fixed instragram photo fetch

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -9,14 +9,9 @@ class InstagramController < ApplicationController
 
   def callback
     response = Instagram.get_access_token(params[:code], :redirect_uri => 'http://localhost:3000/instagram/callback')
-		client = Instagram.client(:access_token => response.access_token)
-    client.user_recent_media.each do |photo|
-      temp_photo = Photo.find_or_initialize_by_url(caption: photo.caption.text, date: photo.caption.created_time.to_i, url:photo.images.standard_resolution.url)
-      temp_photo.update_attributes(lat: photo.location.latitude, long: photo.location.longitude) if photo.location
-      temp_photo.save!
-    end
+		session[:access_token] = response.access_token
 
-    redirect_to user_path(session[:user_id])
+    redirect_to trips_path
   end
 
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,12 +1,23 @@
 class TripsController < ApplicationController
   def index
-
+    @trips = Trip.all
   end
   
   def show
     @user = current_user
     @trip = Trip.find(params[:id])
+
+    # MOVE THIS TO PHOTO CONTROLLER?
     @photos = @trip.photos
+    if @photos.empty?
+      client = Instagram.client(:access_token => session[:access_token])
+      client.user_recent_media.each do |photo|
+        temp_photo = @trip.photos.find_or_initialize_by_url(caption: photo.caption.text, date: photo.caption.created_time.to_i, url:photo.images.standard_resolution.url)
+        temp_photo.update_attributes(lat: photo.location.latitude, long: photo.location.longitude) if photo.location
+        temp_photo.save!
+      end
+      @photos = @trip.photos
+    end
   end
 
   def destroy
