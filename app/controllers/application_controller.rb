5ad00cbbca846e696ae9b58462f2143eff2d9e6b
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-26 15:39:38 -0500

    add readPhotos function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :check_authorization
+  # before_filter :check_authorization
 
 
   private
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
+  # helper_method :current_user
 
   def check_authorization
     unless current_user
