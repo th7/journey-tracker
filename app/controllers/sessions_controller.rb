0de5e8ef95a516b2f47b52a97fb907a803b2a217
dbc-apprentice <patrick.n.moody@gmail.com>
2013-07-27 12:41:31 -0500

    added options to main index view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.from_omniauth(env["omniauth.auth"])
 
     session[:user_id] = user.id
-    redirect_to trips_path
+    redirect_to root_path
   end
 
   def destroy
