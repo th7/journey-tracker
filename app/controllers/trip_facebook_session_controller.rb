f5e9a3657c6d361b69449ae85dc8914bc30d526a
TH <tylerhartland7@gmail.com>
2013-07-31 10:12:46 -0500

    check place_response[0] exists

--- app/controllers/trip_facebook_session_controller.rb
+++ app/controllers/trip_facebook_session_controller.rb
@@ -3,21 +3,22 @@ class TripFacebookSessionController < ApplicationController
   def fetch_photos
 
       user = current_user
-      p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-      p user
+      # p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      # p Time.now
+      # p user
       rest = Koala::Facebook::API.new(user.oauth_token)
       trip = Trip.find(session[:current_trip])
-      p trip.start
-      p trip.end
-      p trip
+      # p trip.start
+      # p trip.end
+      # p trip
       fb_photos = []
 
       # file = open("https://graph.facebook.com/#{user.uid}?fields=albums.limit(5)&access_token=#{user.oauth_token}")
       # data = file.read
       # json_stuff = JSON.parse(data)
-      p "_______________________________________________"
-      p trip.start.to_i
-      p trip.end.to_i
+      # p "_______________________________________________"
+      # p trip.start.to_i
+      # p trip.end.to_i
       # albums = rest.fql_query("SELECT name, aid, created, backdated_time FROM album WHERE (owner = #{user.uid} AND (created > #{trip.start.to_i} OR backdated_time > #{trip.start.to_i}) AND (created < #{trip.end.to_i} OR backdated_time < #{trip.end.to_i} ))")
       albums = rest.fql_query("SELECT name, aid, created, backdated_time FROM album WHERE (owner = #{user.uid} AND (created > #{trip.start.to_i} ) AND (created < #{trip.end.to_i}  ))")
       album_ids = []
@@ -32,7 +33,7 @@ class TripFacebookSessionController < ApplicationController
         # fb_photos << rest.fql_query("SELECT pid, src_big, caption, place_id, backdated_time, created FROM photo WHERE owner = #{user.uid} AND aid = #{album_id} AND (created > #{trip.start.to_i} OR backdated_time > #{trip.start.to_i} ) AND (created < #{trip.end.to_i} OR backdated_time < #{trip.end.to_i} )")
       end
 
-        p "!!!!!!!!!!!!!!!!"+Time.now.to_s
+        # p "!!!!!!!!!!!!!!!!"+Time.now.to_s
       fb_photos << rest.fql_query("SELECT pid, src_big, caption, place_id, backdated_time, created FROM photo WHERE (pid IN (SELECT pid FROM photo_tag WHERE subject=me()) AND (created > #{trip.start.to_i}) AND (created < #{trip.end.to_i}))")
       fb_photos << rest.fql_query("SELECT pid, src_big, caption, place_id, backdated_time, created FROM photo WHERE (owner = me() AND (created > #{trip.start.to_i} ) AND (created < #{trip.end.to_i} ))")
       # fb_photos << rest.fql_query("SELECT pid, src_big, caption, place_id, backdated_time, created FROM photo WHERE (pid IN (SELECT pid FROM photo_tag WHERE subject=me()) AND (created > #{trip.start.to_i} OR backdated_time > #{trip.start.to_i}) AND (created < #{trip.end.to_i} OR backdated_time < #{trip.end.to_i}))")
@@ -40,31 +41,42 @@ class TripFacebookSessionController < ApplicationController
 
   
       
-    p "!!!!!!!!!!!!!!!!!!!!"+Time.now.to_s
       fb_photos.each do |set|
         set.each do |photo|
+          p "!!!!!!!!!!!!!!!!!!!!"+Time.now.to_s
 
           if photo["backdated_time"]
             photo_date = photo["backdated_time"]
           else
             photo_date = photo["created"]
           end
-
+          # p 'photo[place_id]'
+          # p photo['place_id']
           if photo["place_id"]
-            place_response = rest.fql_query("SELECT latitude, longitude FROM place WHERE page_id = #{photo["place_id"]}")
-            place_lat = place_response[0]["latitude"]
-            place_long = place_response[0]["longitude"]
+            # p 'current_user.id'
+            # p current_user.id
+            # p 'fql_query'
+            # p "SELECT latitude, longitude FROM place WHERE page_id = #{photo['place_id']}"
+            place_response = rest.fql_query("SELECT latitude, longitude FROM place WHERE page_id = #{photo['place_id']}")
+            # p 'place_response'
+            # p place_response
+            if place_response[0]
+              place_lat = place_response[0]["latitude"]
+              place_long = place_response[0]["longitude"]
+            end
           end
-
+          p "!!!!!!!!!!!!!!!!!!!!"+Time.now.to_s
           temp_photo = trip.photos.find_or_initialize_by_url(caption: photo["caption"], date: photo_date, url:photo["src_big"])
           temp_photo.update_attributes(lat: place_lat, long: place_long) if photo["place_id"]
           temp_photo.save!
-
+          
         end
       end
 
-p "!!!!!!!!!!!!!!!!!!!!"+Time.now.to_s
+# p "!!!!!!!!!!!!!!!!!!!!"+Time.now.to_s
       # session[:current_trip] = nil
+      # p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      # p Time.now
       redirect_to edit_trip_path(trip)
     end
 
