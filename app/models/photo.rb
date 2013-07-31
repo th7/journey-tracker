cc2f6ccddafa56b87dc3f391a871b3f5b794f350
TH <tylerhartland7@gmail.com>
2013-07-31 10:15:50 -0500

    update specs, remove unused code

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -13,32 +13,11 @@ class Photo < ActiveRecord::Base
   def get_photo_colors
     Miro.options[:color_count] = 6
     colors = Miro::DominantColors.new(self.url).to_hex
-    @palette = self.build_palette(color1: colors[0], color2: colors[1], color3: colors[2], color4: colors[3], color5: colors[4], color6: colors[5])
-    @palette.save
+    self.palette = self.build_palette(color1: colors[0], color2: colors[1], color3: colors[2], color4: colors[3], color5: colors[4], color6: colors[5])
+    self.save
   end
 
   def exif_date=(date)
     self.date = DateTime.strptime(date,"%Y:%m:%d %T").to_i
   end
-
-  def set_gps_as_decimal(array,ref)
-    decimal = array[0].to_f + array[1].to_f/60.0
-    
-    if ref == "N"
-      self.lat = decimal
-    elsif ref == "S"
-      decimal = decimal*-1
-      self.lat = decimal
-    elsif ref == "E"
-      self.long = decimal
-    elsif ref == "W"
-      decimal = decimal*-1
-      # self.update_attributes(long: decimal)
-      self.long = decimal
-    else
-      raise "GPS Ref error"
-    end
-      
-  end
-
 end
