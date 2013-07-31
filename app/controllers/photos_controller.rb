6516920911ec1eb250baa96a1e064e61f62bd59d
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-30 20:20:46 -0500

    updated FBOOK permissions and photo downloads

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -11,13 +11,6 @@ class PhotosController < ApplicationController
 
 	def create
 		@trip = Trip.find(session[:current_trip])
-		p "======= PHOTO PARAMS ==============="
-		p params
-		# {"url"=>"http://i.imgur.com/5drf5AG.jpg", "date"=>"2013:05:04 09:56:14", "lat"=>"N", "lon"=>"E", "action"=>"create", "controller"=>"photos"}
-		# {"url"=>"http://i.imgur.com/cabjMk2.jpg", "date"=>"2013:05:04 09:56:14", "lat"=>["21", "1.77", "0"], "lon"=>["105", "50.91", "0"], "action"=>"create", "controller"=>"photos"}
-		# {"url"=>"http://i.imgur.com/fHHpwBj.jpg", "date"=>"2013:05:04 09:56:14", "lat"=>["21", "1.77", "0"], "lon"=>["105", "50.91", "0"], "latRef"=>"N", "lonRef"=>"E", "action"=>"create", "controller"=>"photos"}
-		# DateTime.strptime("2013:05:04 09:56:14","%Y:%m:%d %T").to_i
-		
 		new_photo = @trip.photos.find_or_initialize_by_url(url: params["url"].gsub!(/(\.)([^\.]*)\z/,'h\1\2'))
 		new_photo.update_attributes(date: DateTime.strptime(params["date"],"%Y:%m:%d %T").to_i)
 
@@ -35,7 +28,6 @@ class PhotosController < ApplicationController
 	end
 
 	def update
-		p params
 		unless params['photo_address'] == "" 
 			formatted_address = params['photo_address'].gsub(/\s/, "+")
 			url = "http://maps.googleapis.com/maps/api/geocode/json?address="+formatted_address+"&sensor=true"
@@ -43,15 +35,9 @@ class PhotosController < ApplicationController
       read_data = file.read
       data = JSON.parse(read_data)
       p params['photo_address']
-      p url
-      p "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-      p data
       latitude = data['results'][0]['geometry']['location']['lat']
-		  p ">>>>>>>>>>>>>>>>>"
-      p latitude
       longitude = data['results'][0]['geometry']['location']['lng']
 		end
-		p params['photo_date'].to_datetime
 		@temp_photo = Photo.find(params['photo_id'])
 		@temp_photo.update_attributes(caption: params["photo_caption"],date: params["photo_date"].to_datetime.to_i)
     
@@ -67,23 +53,6 @@ class PhotosController < ApplicationController
     	render partial: "trips/photo_details", :locals => {:photo => @temp_photo}
     end
 
-    # respond_to do |format|
-    
-
-    # 	# format.json do
-    #  #    @errors = []
-    #  #    if @temp_photo.save!
-    #  #      render :json => {:caption => @temp_photo.caption,
-    #  #                       :lat => @temp_photo.lat,
-    #  #                       :long => @temp_photo.long,
-    #  #                       :date => Time.at(@temp_photo.date),
-    #  #                       :photo_id => @temp_photo.id,
-    #  #                       :trip_id => @temp_photo.trip.id}
-    #  #    else
-    #  #    	render :json => {:error => "Unable to save post"}
-    #  #    end
-    #  #  end
-    # end
 	end
   
   def destroy
