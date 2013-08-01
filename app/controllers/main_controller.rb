5bc41d9966c0afb79cf25ae217f719ca4a5b377f
patmood <patrick.n.moody@gmail.com>
2013-08-01 10:03:32 -0500

    fixed main redirect

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,8 @@ class MainController < ApplicationController
   skip_before_filter :check_authorization, only: [:index]
 
   def index
-  	redirect_to new_trip_path if current_user.trips
+  	if current_user	
+	  	redirect_to new_trip_path if current_user.nil?
+	  end
   end
 end
