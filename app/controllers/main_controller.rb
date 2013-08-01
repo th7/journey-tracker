6e950d80ce5cd237f51a9a2403a75a884f498612
patmood <patrick.n.moody@gmail.com>
2013-08-01 09:29:29 -0500

    minor fixes, added show trip button

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,6 @@ class MainController < ApplicationController
   skip_before_filter :check_authorization, only: [:index]
 
   def index
-    p session
+  	redirect_to new_trip_path if current_user.trips
   end
 end
