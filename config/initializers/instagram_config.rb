4692773be435d6ff1da089af9d10d2c9b64561b1
patmood <patrick.n.moody@gmail.com>
2013-08-13 17:09:43 -0500

    instagram callback now based on Rails.env. Also tidied up debug puts statements

--- config/initializers/instagram_config.rb
+++ config/initializers/instagram_config.rb
@@ -1,4 +1,9 @@
-INSTAGRAM_CALLBACK_URL = 'http://localhost:3000/instagram/callback'
+if Rails.env == "development" || Rails.env == "test"
+  INSTAGRAM_CALLBACK_URL = 'http://localhost:3000/instagram/callback'
+else
+  INSTAGRAM_CALLBACK_URL = 'http://viagg.io/instagram/callback'
+end
+
 Instagram.configure do |config|
   config.client_id = ENV['INSTAGRAM_ID']
   config.client_secret = ENV['INSTAGRAM_SECRET']
